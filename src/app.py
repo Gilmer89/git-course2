d5504773389a09f2dcb9ad987a19ee94917be7c8
gilmer <gilmerperez4@gmail.com>
2022-07-07 00:25:29 -0500

    Feature: agregando texto a la suma. AP

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def index():
     return "hello world!"
 
-@app.route('/suma<int:a>/<int:b>')
+@app.route('/sum/<int:a>/<int:b>')
 def sum(a: int, b: int):
-    return a+b
+    nums_sum= a+b
+    return str(nums_sum)
